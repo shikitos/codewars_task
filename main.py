837e598e570db5d070e26ac3a9cdb6dd2a4da61e
Shikitos <shitovn16@gmail.com>
2022-10-24 00:04:17 +0200

    Simplify the code

--- main.py
+++ main.py
@@ -1,71 +1,36 @@
-from typing import Dict
-
-
 # https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/python
-
 def format_duration(seconds):
-    list_of_durations: dict[str, int] = {
+    list_of_durations = {
         "minute": 60,
         "hour": 60 * 60,
         "day": 24 * 3600,
         "year": 24 * 3600 * 365
     }
-
     result = ''
-    word_ending = ''
+    year = list_of_durations["year"]
+    day = list_of_durations["day"]
+    hour = list_of_durations["hour"]
+    minute = list_of_durations["minute"]
 
-    # less than 1 hour but more than 1 minute
-    if list_of_durations["minute"] <= seconds < list_of_durations["hour"]:
-        return
-
-    # greater than 1 hour (DONE)
-    if list_of_durations["hour"] <= seconds < list_of_durations["day"]:
-        # hours
-        if seconds / list_of_durations["hour"] >= 2:
-            word_ending = 's'
-        result = f'{int(seconds / list_of_durations["hour"])} hour{word_ending}'
-        word_ending = ''
-        # minutes
-        if seconds % list_of_durations["hour"] / 60 >= 1:
-            if seconds % list_of_durations["hour"] / 60 >= 2:
-                word_ending = 's'
-            result += f', {int(seconds % list_of_durations["hour"] / 60)} minute{word_ending}'
-        word_ending = ''
-        # seconds
-        if seconds % list_of_durations["hour"] % list_of_durations["minute"] != 0:
-            if seconds % list_of_durations["hour"] % list_of_durations["minute"] > 1:
-                word_ending = 's'
-            result += f' and {seconds % list_of_durations["hour"] % list_of_durations["minute"]} second{word_ending}'
-        return result
-
-    # days should be here
-
-    # TODO : I have a problem identifying the HOURS correctly
-    # if seconds value less than inf and greater than day, i.e. years
-    if list_of_durations["day"] <= seconds < list_of_durations["year"]:
-        # days
-        if seconds / list_of_durations["day"] >= 2:
-            word_ending = 's'
-        result += f'{int(seconds / list_of_durations["day"])} day{word_ending}'
-        word_ending = ''
-        # hours
-        if seconds > list_of_durations['hour']:
-            if seconds / list_of_durations["hour"] >= 2:
-                word_ending = 's'
-            result += f', {int(seconds / list_of_durations["hour"])} hour{word_ending}'
-        word_ending = ''
-        # minutes
-        if seconds % list_of_durations["hour"] / 60 >= 1:
-            if seconds % list_of_durations["hour"] / 60 >= 2:
-                word_ending = 's'
-            result += f', {int(seconds % list_of_durations["hour"] / 60)} minute{word_ending}'
-        word_ending = ''
-        # seconds
-        if seconds % list_of_durations["hour"] % list_of_durations["minute"] != 0:
-            if seconds % list_of_durations["hour"] % list_of_durations["minute"] > 1:
-                word_ending = 's'
-            result += f' and {seconds % list_of_durations["hour"] % list_of_durations["minute"]} second{word_ending}'
-        return result
+    # years
+    if seconds >= year:
+        result += f'{int(seconds / year)} year{"s" if seconds / year >= 2 else ""}'
+        remainder = seconds - int(seconds / year) * year
+        print(int(seconds / year) * year)
+        print(remainder)
+    # days
+    if seconds >= day:
+        result += f'{int(seconds / day) - int(seconds / year) * 365} day{"s" if seconds / day >= 2 else ""}'
+    # hours
+    if seconds >= list_of_durations['hour']:
+        result += f'{int(seconds % day / hour)} hour{"s" if seconds / hour > 1 else ""}'
+    # minutes
+    if seconds % hour / 60 >= 1:
+        result += f'{int(seconds % hour / 60)} minute{"s" if seconds % hour / 60 >= 2 else ""}'
+    # seconds
+    if seconds % hour % minute != 0:
+        result += f'{int(seconds % hour % minute)} second{"s" if seconds % hour % minute > 1 else ""}'
+    return result
 
 
 print(format_duration(1))  # 1 second
@@ -73,5 +38,6 @@ print(format_duration(62))  # 1 minute and 2 seconds
 print(format_duration(120))  # 2 minutes
 print(format_duration(3600))  # 1 hour
 print(format_duration(7264))  # 2 hours, 1 minute and 4 seconds
-print(format_duration(75454681))  # 2 years, 2919 hours, 26 minutes and 39 seconds
-print(format_duration(10509999))  # 121 days, 15 hours, 26 minutes and 38 seconds
+print(format_duration(90000))  # 1 day and 1 hour
+print(format_duration(10509999))  # 121 days, 15 hours, 26 minutes and 39 seconds
+print(format_duration(75454681))  # 2 years, 143 days, 7 hours, 38 minutes and 1 seconds
